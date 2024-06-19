e7bafdb6e9324907ea23d05ee1dabf023c024805
AryanAwtar <aryanshankar302@outlook.com>
2024-06-19 16:09:51 +0530

    bugs fixed

--- Web-Dev/projects/project-1-Dice/index.js
+++ Web-Dev/projects/project-1-Dice/index.js
@@ -1,4 +1,7 @@
-document.querySelector('.refresh').addEventListener("click",function(){{    let randomNumber1=Math.floor(Math.random()*6)+1;
+document.querySelector('.refresh').addEventListener("click",function(){{  
+    const element = document.getElementById("refresh");  // Get the DIV element
+    element.classList.remove("refresh");
+    let randomNumber1=Math.floor(Math.random()*6)+1;
     let randomImageSource1="images/dice"+randomNumber1+".png";
     document.querySelectorAll("img")[0].setAttribute("src",randomImageSource1);
 
@@ -7,12 +10,12 @@ document.querySelector('.refresh').addEventListener("click",function(){{    let 
     document.querySelectorAll("img")[1].setAttribute("src",randomImageSource2);
 
     if(randomNumber1>randomNumber2){
-        document.querySelector("h1").innerHTML="🚩 Player 1 Wins! <br> Refresh Again";
+        document.querySelector("h1").innerHTML='🚩 Player 1 Wins! <br> <span class="refresh" style="font-size:2 rem; color: blanchedalmond;">Play Again</span>';
     }
     else if(randomNumber2>randomNumber1){   
-        document.querySelector("h1").innerHTML="Player 2 Wins! 🚩<br> Refresh Again";
+        document.querySelector("h1").innerHTML='Player 2 Wins! 🚩<br> <span class="refresh" style="font-size:2 rem; color: blanchedalmond;">Play Again</span>';
     }
     else{   
-        document.querySelector("h1").innerHTML="Draw!<br> Refresh Again";
+        document.querySelector("h1").innerHTML='Draw!<br> <span class="refresh" style="font-size:2 rem; color: blanchedalmond;">Play Again</span>';
     }
 }})
