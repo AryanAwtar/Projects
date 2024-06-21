ec6b8bd83ba86f39e05f6232c38fc2cd5966b406
AryanAwtar <aryanshankar302@outlook.com>
2024-06-21 22:57:40 +0530

    adding sounds

--- Web-Dev/projects/project-3-SimonGame/game.js
+++ Web-Dev/projects/project-3-SimonGame/game.js
@@ -1,10 +1,44 @@
 let gamePatten=[];
 let buttonColors=["red","blue","green","yellow"];
-let randomChosenColour=buttonColors[nextSequence()];
+let randomIndex = nextSequence();
+let randomChosenColour = buttonColors[randomIndex];
 gamePatten.push(randomChosenColour);
-animateButton(gamePatten);
+
+
+soundButton(randomChosenColour);
+animateButton(randomChosenColour);
+
+
 function soundButton(buttonColor){
-    
+    switch(buttonColor){
+        case "red":
+            let red = new Audio("sounds/red.mp3");
+            red.play();
+            break;
+        case "blue":
+            let blue = new Audio("sounds/blue.mp3");
+            blue.play();
+            break;
+        case "green":
+            let green = new Audio("sounds/green.mp3");
+            green.play();
+            break;
+        case "yellow":
+            let yellow = new Audio("sounds/yellow.mp3");
+            yellow.play();
+            break;
+        default:
+            console.log(buttonColor);
+            let def = new Audio("sounds/wrong.mp3");
+            def.play();
+    } 
+}
+
+function playAudio(audioPath) {
+    const audio = new Audio(audioPath);
+    audio.play().catch(error => {
+        console.log("Failed to play audio:", error);
+    });
 }
 function animateButton(buttonColor) {
     $("#" + buttonColor).fadeIn(100).fadeOut(100).fadeIn(100);
@@ -17,3 +51,7 @@ function nextSequence(){
 console.log(nextSequence());   
 console.log(buttonColors[2]);  
 console.log(gamePatten);     
+console.log(randomChosenColour);
+console.log(randomIndex);
+console.log(gamePatten[0]);
+console.log(gamePatten.length);
