85287016af4d120c4324e8e4e27ff91b84aaa881
AryanAwtar <aryanshankar302@outlook.com>
2024-06-22 12:39:43 +0530

    using ans form udemy

--- Web-Dev/projects/project-3-SimonGame/game.js
+++ Web-Dev/projects/project-3-SimonGame/game.js
@@ -1,70 +1,59 @@
-let gamePatten=[];
-let userClickedPattern=[];
-let buttonColors=["red","blue","green","yellow"];
-let randomIndex = nextSequence();
-let randomChosenColour = buttonColors[randomIndex];
-gamePatten.push(randomChosenColour);
 
+var buttonColours = ["red", "blue", "green", "yellow"];
 
-for(let i=0;i<document.querySelectorAll(".btn").length;i++){
-    document.querySelectorAll(".btn")[i].addEventListener("click",function(){
-        let userChosenColour = this.id;
-        userClickedPattern.push(userChosenColour);
-        console.log(userClickedPattern);
-        soundButton(userChosenColour);
-        animateButton(userChosenColour);
-        
-    })  
+var gamePattern = [];
+var userClickedPattern = [];
+
+//You'll need a way to keep track of whether if the game has started or not, so you only call nextSequence() on the first keypress.
+var started = false;
+
+//2. Create a new variable called level and start at level 0.
+var level = 0;
+
+//1. Use jQuery to detect when a keyboard key has been pressed, when that happens for the first time, call nextSequence().
+$(document).keypress(function() {
+  if (!started) {
+
+    //3. The h1 title starts out saying "Press A Key to Start", when the game has started, change this to say "Level 0".
+    $("#level-title").text("Level " + level);
+    nextSequence();
+    started = true;
+  }
+});
+
+$(".btn").click(function() {
+
+  var userChosenColour = $(this).attr("id");
+  userClickedPattern.push(userChosenColour);
+
+  playSound(userChosenColour);
+  animatePress(userChosenColour);
+});
+
+function nextSequence() {
+
+  //4. Inside nextSequence(), increase the level by 1 every time nextSequence() is called.
+  level++;
+
+  //5. Inside nextSequence(), update the h1 with this change in the value of level.
+  $("#level-title").text("Level " + level);
+
+  var randomNumber = Math.floor(Math.random() * 4);
+  var randomChosenColour = buttonColours[randomNumber];
+  gamePattern.push(randomChosenColour);
+
+  $("#" + randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);
+  playSound(randomChosenColour);
 }
 
-document.addEventListener("click",function(){{  
-soundButton(randomChosenColour);
-animateButton(randomChosenColour);
-}})
-document.addEventListener("keypress",function(event){
-    soundButton(randomChosenColour);
-    animateButton(randomChosenColour);
-})
-function soundButton(buttonColor){
-    switch(buttonColor){
-        case "red":
-            let red = new Audio("sounds/red.mp3");
-            red.play();
-            break;
-        case "blue":
-            let blue = new Audio("sounds/blue.mp3");
-            blue.play();
-            break;
-        case "green":
-            let green = new Audio("sounds/green.mp3");
-            green.play();
-            break;
-        case "yellow":
-            let yellow = new Audio("sounds/yellow.mp3");
-            yellow.play();
-            break;
-        default:
-            console.log(buttonColor);
-            let def = new Audio("sounds/wrong.mp3");
-            def.play();
-            break;
-    } 
+function playSound(name) {
+  var audio = new Audio("sounds/" + name + ".mp3");
+  audio.play();
 }
 
-function playAudio(audioPath) {
-    const audio = new Audio(audioPath);
-    audio.play().catch(error => {
-        console.log("Failed to play audio:", error);
-    });
+function animatePress(currentColor) {
+  $("#" + currentColor).addClass("pressed");
+  setTimeout(function () {
+    $("#" + currentColor).removeClass("pressed");
+  }, 100);
 }
-function animateButton(buttonColor) {
-    $("#" + buttonColor).fadeIn(100).fadeOut(100).fadeIn(100);
-}
-function nextSequence(){
-    let randomNumber = Math.floor(Math.random()*4);
-    return randomNumber;
-}
-
-console.log(nextSequence());   
-console.log(buttonColors[2]);  
-console.log(gamePatten);
